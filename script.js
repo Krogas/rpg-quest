4da07335d19a767a83d4be33fdc257169a182032
Maciej Zawadzki <94796152+Krogas@users.noreply.github.com>
2026-06-19 22:39:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const pages = [
+
+`Cześć.
+
+Nazywam się Kasia.
+
+To jest mój QUEST LOG.`,
+
+`Zaginął mój mąż.
+
+Ostatni raz widziany był w miejscu,
+gdzie zaczyna się zabawa i kończy rozsądek.`,
+
+`Twoim zadaniem będzie:
+- odnaleźć ślady
+- podjąć decyzję
+- wejść w historię`,
+
+`Jebać biede`
+
+
+];
+
+const textEl = document.getElementById("text");
+const nextBtn = document.getElementById("nextBtn");
+
+let page = 0;
+
+function type(text, cb){
+
+    textEl.innerHTML = "";
+
+    let i = 0;
+
+    function tick(){
+
+        if(i < text.length){
+
+            textEl.innerHTML =
+                text.substring(0,i+1) +
+                '<span class="cursor">▍</span>';
+
+            i++;
+            setTimeout(tick, 25);
+
+        } else {
+            textEl.innerHTML = text;
+            cb();
+        }
+    }
+
+    tick();
+}
+
+function showPage(i){
+
+    nextBtn.classList.remove("hidden");
+
+    type(pages[i], () => {
+        nextBtn.classList.remove("hidden");
+    });
+}
+
+nextBtn.addEventListener("click", () => {
+
+    page++;
+
+    if(page < pages.length){
+        showPage(page);
+    } else {
+        showConsent();
+    }
+});
+
+function showConsent(){
+document.getElementById("actions").style.display = "none";
+    textEl.innerHTML = `
+        <div class="quest-panel">
+            <h2>⚠ WARUNKI QUESTU</h2>
+            <div class="quest-line">
+                Zgoda wiąże się ze zgodą na udostępnienie wizerunku.
+            </div>
+            <button class="quest-btn" onclick="finish()">
+                ACCEPT QUEST
+            </button>
+
+        </div>
+    `;
+}
+
+
+window.finish = function(){
+
+   
+    const code =
+        "Akceptuje zadanie młody parawanie";
+
+    textEl.innerHTML = `
+        <div class="final">
+            ✔ QUEST ACCEPTED
+
+            Tajny KOD POWIEDZ TO NA GŁOS: <b>${code}</b>
+
+
+        </div>
+    `;
+};
+
+showPage(0);
+
+});
